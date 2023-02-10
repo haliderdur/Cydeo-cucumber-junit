5ad5264b58fbdcd07c0455db357925efb496a358
Halid Erdur <haliderdur@gmail.com>
2023-02-10 17:20:30 +0000

    Hooks and Background

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -12,9 +12,19 @@ import io.cucumber.java.BeforeStep;
 
 public class Hooks {
     // import from io.cucumber.java, not from junit
-    @Before
+    @Before(order = 0)
     public void setupScenario() {
         System.out.println("====> Setting up browser using cucumber @Before");
+    }
+
+    @Before(value = "@login", order = 1)
+    public void setupForLoginsScenario() {
+        System.out.println("====> This will only apply to scenarios with @login tag");
+    }
+
+    @Before("@database")
+    public void setupForDatabaseScenario() {
+        System.out.println("====> This will only apply to scenarios with @databese tag");
     }
 
     @After
--- src/test/resources/features/login.feature
+++ src/test/resources/features/login.feature
@@ -1,4 +1,4 @@
-@regression @smoke
+@regression @smoke @login
 Feature: Library app login feature
   Agile story: As a user, I should be able to login with correct credentials to different
   accounts. And dashboard should be displayed.
